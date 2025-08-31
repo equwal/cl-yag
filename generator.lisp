8d6b5bf244f382c1bda11d4f05d1b5a0fcc77d43
Spenser Truex <truex@equwal.com>
2025-08-31 18:30:17 -0400

    Unintended reduplication

--- generator.lisp
+++ generator.lisp
@@ -121,8 +121,7 @@
 ;; get the converter object of "article"
 (defmacro with-converter(&body code)
   `(progn
-     (let ((converter-name (if (article-converter article)
-			       (article-converter article)
+     (let ((converter-name (or (article-converter article)
 			     (getf *config* :default-converter))))
        (let ((converter-object (getf *converters* converter-name)))
 	 ,@code))))
@@ -132,7 +131,6 @@
 (defun use-converter-to-html(filename &optional (converter-name nil))
   (let* ((converter-object (getf *converters*
                                  (or converter-name
-			             converter-name
 			             (getf *config* :default-converter))))
          (output           (converter-command converter-object))
          (src-file (format nil "~a~a" filename (converter-extension converter-object)))
@@ -266,7 +264,7 @@
 ;; produce the code of a whole page with title+layout with the parameter as the content
 (defun generate-layout(body &optional &key (title nil))
   (prepare "templates/layout.tpl"
-	   (template "%%Title%%" (if title title (getf *config* :title)))
+	   (template "%%Title%%" (or title (getf *config* :title)))
 	   (template "%%Tags%%" (get-tag-list))
 	   (template "%%Body%%" body)
 	   output))
