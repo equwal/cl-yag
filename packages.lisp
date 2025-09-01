c8a13b65aae06b43fa193df726c14f2afa2d1794
Spenser Truex <truex@equwal.com>
2025-09-01 16:08:09 -0400

    Just use one package

--- packages.lisp
+++ packages.lisp
@@ -1,24 +1,3 @@
-
-(in-package :cl-yag-system)
-
-(defpackage #:html-generator
-   (:use #:cl)
-   (:export #:create-html-site
-            #:generate-rss-html))
-
-(defpackage #:gopher-generator
-   (:use #:cl)
-   (:export #:create-gopher-hole
-            #:generate-rss-gopher))
-
 (defpackage #:cl-yag
    (:use #:cl)
-   (:import-from #:html-generator
-
-                 #:create-html-site
-                 #:generate-rss-html)
-   (:import-from #:gopher-generator
-
-                 #:create-gopher-hole
-                 #:generate-rss-gopher)
    (:export #:generate-site))
