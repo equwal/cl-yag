d122eed76a9fcb1e083e428c159834cb18c936cd
Spenser Truex <truex@equwal.com>
2025-09-01 16:24:53 -0400

    Remove merge conflicts

--- generators-util.lisp
+++ generators-util.lisp
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-(in-package #:cl-yag)
-=======
 
 (in-package :cl-yag)
 
@@ -8,19 +5,12 @@
   "Simplify file saving by using the layout system."
   `(progn
      (save-file ,name (generate-layout ,@data))))
->>>>>>> abstract-protocols
 
 (defmacro template(before &body after)
   "Simplify string replacement work in templates."
   `(progn
      (setf output (replace-all output ,before ,@after))))
 
-<<<<<<< HEAD
-(defmacro generate(name &body data)
-  "Simplify file saving by using the layout system."
-  `(progn
-     (save-file ,name (generate-layout ,@data))))
-=======
 (defmacro with-converter(&body code)
   "Get the converter object for an article and execute code in that context."
   `(progn
@@ -28,5 +18,3 @@
 			     (getf *config* :default-converter))))
        (let ((converter-object (getf *converters* converter-name)))
 	 ,@code))))
-
->>>>>>> abstract-protocols
